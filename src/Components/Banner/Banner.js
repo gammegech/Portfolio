60473b35442c96ca79198808f0fd77b9fc64e3fd
gammegech <gammegech1887@gmail.com>
2024-07-10 00:29:06 +0300

    no message

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Banner = () => {
   return (
-    <section className='w-full h-[800px] pb-20 flex border-b-[1px] border-b-black'>
-      
+    <section id='home' className='w-full h-[800px] pb-20 flex border-b-[1px] border-b-black'>
+      Banner
     </section>
   )
 }
