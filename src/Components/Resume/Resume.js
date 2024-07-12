36e76278ebd262c43474d022f211b33d8937b654
gammegech <gammegech1887@gmail.com>
2024-07-13 00:04:26 +0300

    resume

--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Titles from "../Layouts/Titles";
+import ResumeCard from "./ResumeCard";
 
 const Resume = () => {
   return (
@@ -14,6 +15,30 @@ const Resume = () => {
           <li className="resumeLi">Experience</li>
           <li className="resumeLi">Achievement</li>
         </ul>
+      </div>
+      <div className="py-12 font-titleFont">
+        <p className="text-sm text-designColor tracking-[4px]">1998-2010</p>
+        <h2 className="text-4xl font-bold">Education Quality</h2>
+      </div>
+      <div className="mt-14 w-1/2 h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+        <ResumeCard
+          title="Ms in Data Science"
+          subTitle="Osiri University (2024 - 2025)"
+          result="3.75/4"
+          des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+        />
+        <ResumeCard
+          title="BSc in Software Engineering"
+          subTitle="Haramaya University (2013 - 2016)"
+          result="3.07/4"
+          des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+        />
+        <ResumeCard
+          title="Secondary School Education"
+          subTitle="Bilo Secondary School (2009 - 2012)"
+          result="5.00/5"
+          des="Secondary Education or post-primary education covers two phases on the International Startard Classification of Education Scale"
+        />
       </div>
     </section>
   );
---
+++ src/Components/Resume/ResumeCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const ResumeCard = () => {
+  return (
+    <div>
+      <div className="w-full h-1/3 bg-blue-400">
+       <div className="w-10 h-[6px] bg-opacity-0 mt-16 relative"></div>
+       <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default ResumeCard;
