65ba4fb5832a445d813adc474732f87bbe54bd1c
gammegech <gammegech1887@gmail.com>
2024-07-17 12:26:04 +0300

    resume all link

---
+++ src/Components/Resume/Achievements.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import ResumeCard from './ResumeCard';
+
+const Achievements = () => {
+  return (
+    <div className="w-full flex gap-20">
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px]">2009-2016</p>
+          <h2 className="text-4xl font-bold">Company Experience</h2>
+        </div>
+        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Ms in Data Science"
+            subTitle="Osiri University (2024 - 2025)"
+            result="3.75/4"
+            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+          />
+          <ResumeCard
+            title="BSc in Software Engineering"
+            subTitle="Haramaya University (2013 - 2016)"
+            result="3.07/4"
+            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+          />
+          <ResumeCard
+            title="Secondary School Education"
+            subTitle="Bilo Secondary School (2009 - 2012)"
+            result="5.00/5"
+            des="Secondary Education or post-primary education covers two phases on the International Startard Classification of Education Scale"
+          />
+        </div>
+      </div>
+      <div>
+        <div className="py-12 font-titleFont">
+          <p className="text-sm text-designColor tracking-[4px]">2017-2024</p>
+          <h2 className="text-4xl font-bold">Job Experience</h2>
+        </div>
+        <div className="mt-14 w-full h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
+          <ResumeCard
+            title="Ms in Data Science"
+            subTitle="Osiri University (2024 - 2025)"
+            result="3.75/4"
+            des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
+          />
+          <ResumeCard
+            title="BSc in Software Engineering"
+            subTitle="Haramaya University (2013 - 2016)"
+            result="3.07/4"
+            des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
+          />
+          <ResumeCard
+            title="Secondary School Education"
+            subTitle="Bilo Secondary School (2009 - 2012)"
+            result="5.00/5"
+            des="Secondary Education or post-primary education covers two phases on the International Startard Classification of Education Scale"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Achievements
--- src/Components/Resume/Resume.js
+++ src/Components/Resume/Resume.js
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Titles from "../Layouts/Titles";
 import ResumeCard from "./ResumeCard";
+import Education from "./Education";
+import Skills from "./Skills";
+import Experience from "./Experience";
+import Achievements from "./Achievements";
 
 const Resume = () => {
+  const [educationData, setEducationData] = useState(true);
+  const [skillData, setSkillData] = useState(false);
+  const [experienceData, setExperienceData] = useState(false);
+  const [achievementData, setAchievementData] = useState(false);
+
   return (
     <section id="resume" className="w-full py-20 border-b-[1px] border-b-black">
       <div className="flex justify-center items-center text-center">
@@ -10,36 +19,56 @@ const Resume = () => {
       </div>
       <div>
         <ul className="w-full grid grid-cols-4">
-          <li className="resumeLi">Education </li>
-          <li className="resumeLi">Professinal Skills</li>
-          <li className="resumeLi">Experience</li>
-          <li className="resumeLi">Achievement</li>
+          <li
+            onClick={() =>
+              setEducationData(true) &
+              setSkillData(false) &
+              setExperienceData(false) &
+              setAchievementData(false)
+            }
+            className="resumeLi"
+          >
+            Education
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(true) &
+              setExperienceData(false) &
+              setAchievementData(false)
+            }
+            className="resumeLi"
+          >
+            Professinal Skills
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(false) &
+              setExperienceData(true) &
+              setAchievementData(false)
+            }
+            className="resumeLi"
+          >
+            Experience
+          </li>
+          <li
+            onClick={() =>
+              setEducationData(false) &
+              setSkillData(false) &
+              setExperienceData(false) &
+              setAchievementData(true)
+            }
+            className="resumeLi"
+          >
+            Achievement
+          </li>
         </ul>
       </div>
-      <div className="py-12 font-titleFont">
-        <p className="text-sm text-designColor tracking-[4px]">1998-2010</p>
-        <h2 className="text-4xl font-bold">Education Quality</h2>
-      </div>
-      <div className="mt-14 w-1/2 h-[1000px] border-l-[6px] border-l-black border-opacity-30 flex flex-col gap-10">
-        <ResumeCard
-          title="Ms in Data Science"
-          subTitle="Osiri University (2024 - 2025)"
-          result="3.75/4"
-          des="Higher education is tertiary education leading to award of an academic degree. Higher education, also called post-secondary education."
-        />
-        <ResumeCard
-          title="BSc in Software Engineering"
-          subTitle="Haramaya University (2013 - 2016)"
-          result="3.07/4"
-          des="The training provided by universities in order to prepare people to work in various sectors of the economy or areas of culture."
-        />
-        <ResumeCard
-          title="Secondary School Education"
-          subTitle="Bilo Secondary School (2009 - 2012)"
-          result="5.00/5"
-          des="Secondary Education or post-primary education covers two phases on the International Startard Classification of Education Scale"
-        />
-      </div>
+      {educationData && <Education />}
+      {skillData && <Skills />}
+      {experienceData && <Experience />}
+      {achievementData && <Achievements />}
     </section>
   );
 };
